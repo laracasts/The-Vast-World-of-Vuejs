e9dc3d0191dd762aa71ef9f3b4defae9b189c12b
JeffreyWay <jeffrey@jeffrey-way.com>
2015-05-21 19:31:22 -0400

    Lesson 1 complete

---
+++ app.js
@@ -0,0 +1,77 @@
+new Vue({
+
+    el: '#tasks',
+
+    data: {
+        tasks: [],
+
+        newTask: '',
+
+        filters: {
+            inProcess: function(task) {
+                return ! task.completed;
+            },
+
+            completed: function(task) {
+                return task.completed;
+            }
+        }
+    },
+
+    computed: {
+        completions: function() {
+            return this.tasks.filter(this.filters.completed);
+        },
+
+        remaining: function() {
+            return this.tasks.filter(this.filters.inProcess);
+        }
+    },
+
+    filters: {
+        inProcess: function(tasks) {
+            return tasks.filter(this.filters.inProcess);
+        }
+    },
+
+    methods: {
+        addTask: function(e) {
+            e.preventDefault();
+
+            if ( ! this.newTask) return;
+
+            this.tasks.push({
+                body: this.newTask,
+                completed: false
+            });
+
+            this.newTask = '';
+        },
+
+        editTask: function(task) {
+            this.removeTask(task);
+            this.newTask = task.body;
+
+            this.$$.newTask.focus();
+        },
+
+        toggleTaskCompletion: function(task) {
+            task.completed = ! task.completed;
+        },
+
+        completeAll: function() {
+            this.tasks.forEach(function(task) {
+                task.completed = true;
+            });
+        },
+
+        clearCompleted: function() {
+            this.tasks = this.tasks.filter(this.filters.inProcess);
+        },
+
+        removeTask: function(task) {
+            this.tasks.$remove(task);
+        }
+    }
+
+});
